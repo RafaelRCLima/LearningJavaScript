86a211c656c8447bd4463c2568f73b964f788d58
RafaelRCLima <rafael.lima19-92@hotmail.com>
2020-02-27 17:04:00 -0300

    Conceitos de herança

---
+++ objetos/heranca2.js
@@ -0,0 +1,49 @@
+// Cadeia de protótipos
+Object.prototype.attr0 = 'Z' // Evitar
+
+const avo = { attr1: 'A' }
+const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
+const filho = { __proto__: pai, attr3: 'C' }
+
+console.log(filho.attr0)
+console.log(filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelerarMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual += delta
+        } else {
+            this.velAtual = this.velMax
+        }
+    },
+    status() {
+        return `${this.velAtual}km/h de ${this.velMax}km/h`
+    }
+}
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 //shadowing -- 'sobrescreve' o valor definido pelo obj pai
+}
+
+const volvo = {
+    modelo: 'V40',
+    velMax: 200,
+    status() {
+        return `${this.modelo}: ${super.status()}`
+    }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelerarMais(100)
+console.log(volvo.status())
+
+ferrari.acelerarMais(250)
+console.log(ferrari.status())
