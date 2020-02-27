e69156eeadfa0ae422c8b7025f7a4798f6ef8aa0
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima19-92@hotmail.com>
2020-02-27 13:47:06 -0300

    Revisão de objetos

--- Functions/factoryFunction.js
+++ Functions/factoryFunction.js
@@ -10,3 +10,21 @@ const cortadorDeUnha = new Produto('Cortador de Unha', 3.5)
 
 console.log(cortadorDeUnha)
 console.log(Produto('Notebook', 3000.00))
+
+function Pessoa(nome, idade) {
+    return {
+        nome,
+        idade,
+        envelhecer: function(){
+            this.idade++
+        }
+    }
+}
+
+const Joao = new Pessoa('Joao', 28)
+
+for (let i = 1; i < 8; i++){
+    Joao.envelhecer()
+}
+
+console.log(Joao)
--- Functions/thisEBind.js
+++ Functions/thisEBind.js
@@ -1,37 +1,74 @@
-const pessoa = {
-    saudacao: 'Bom dia!',
-    falar() {
-        console.log(this.saudacao)
+// const pessoa = {
+//     saudacao: 'Bom dia!',
+//     falar() {
+//         console.log(this.saudacao)
+//     }
+// }
+
+// pessoa.falar()
+// const falar = pessoa.falar
+// falar() // conflito entre paradigmas: funcional e OO
+
+// const falarDePessoa = pessoa.falar.bind(pessoa)
+// falarDePessoa()
+
+// //===================================================
+
+// function Pessoa() {
+//     this.idade = 0
+//     setInterval(function () {
+//         this.idade++
+//         console.log(this.idade)
+//     }.bind(this), 1000)
+// }
+
+// //new Pessoa
+
+// function Pessoa2() {
+//     this.idade = 0
+//     const self = this
+//     setInterval(function () {
+//         self.idade++
+//         console.log(self.idade)
+//     }, 1000)
+// }
+
+//new Pessoa2
+
+const id = 3
+
+const obj = {
+    id: 1,
+    increm: function (id){
+        id = id + this.id
+        this.id++
+        console.log(id)
     }
 }
 
-pessoa.falar()
-const falar = pessoa.falar
-falar() // conflito entre paradigmas: funcional e OO
-
-const falarDePessoa = pessoa.falar.bind(pessoa)
-falarDePessoa()
-
-//===================================================
-
-function Pessoa() {
-    this.idade = 0
-    setInterval(function () {
-        this.idade++
-        console.log(this.idade)
-    }.bind(this), 1000)
+const obj2 = {
+    id: 1,
+    falar: () => {
+        console.log('Aqui', ++obj2.id)
+    }
 }
 
-//new Pessoa
 
-function Pessoa2() {
-    this.idade = 0
-    const self = this
-    setInterval(function () {
-        self.idade++
-        console.log(self.idade)
-    }, 1000)
+console.log(obj.id)
+
+obj.increm(id)
+
+console.log(obj.id)
+
+obj2.falar()
+console.log('qquer',obj2.id)
+
+let valor = 3
+
+const funcao = () => {
+    valor++
 }
 
-new Pessoa2
+funcao()
+console.log(valor)
 
